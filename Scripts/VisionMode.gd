679c0abe1d9c02c544a208a6d26961577a0147a6
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-08-01 16:15:22 -0300

    lights off in nightvision

--- Scripts/VisionMode.gd
+++ Scripts/VisionMode.gd
@@ -8,11 +8,13 @@ func _ready():
 	color = DARK
 
 func NightVision_mode():
+	inform_npcs("NightVision_mode")
 	color = NIGHTVISION
 	$AudioStreamPlayer2D.stream = load(Global.nightvision_on_sfx)
 	play_sfx()
 
 func DarkVision_mode():
+	inform_npcs("DarkVision_mode")
 	color = DARK
 	$AudioStreamPlayer2D.stream = load(Global.nightvision_off_sfx)
 	play_sfx()
@@ -20,7 +22,8 @@ func DarkVision_mode():
 func play_sfx():
 	$AudioStreamPlayer2D.play()
 
-
+func inform_npcs(vision_mode):
+	get_tree().call_group("npc", vision_mode)
 
 
 
